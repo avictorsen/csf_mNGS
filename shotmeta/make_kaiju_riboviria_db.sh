eb14e9de79e589396e9c986e34adbcc931c6829f
Alec Victorsen <victo160@umn.edu>
2023-07-19 13:36:49 -0500

    20230719-3

--- shotmeta/make_kaiju_riboviria_db.sh
+++ shotmeta/make_kaiju_riboviria_db.sh
@@ -11,20 +11,15 @@ while read line; do
   fi;
 done < Riboviria.txt
 
-## added after talking with Zach
-mkdir all
-KRAKEN2_PROTEIN_DB=FALSE
-KRAKEN2_USE_FTP=FALSE
+perl modified_kraken2_rsync_from_ncbi.pl shortlist.txt
 
-#perl modified_kraken2_rsync_from_ncbi.pl shortlist.txt
+mkdir source
+cd source
+wget http://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
 
-#mkdir source
-#cd source
-#wget http://ftp.ncbi.nlm.nih.gov/pub/taxonomy/taxdump.tar.gz
+cat library.fna | awk '{if ($0 ~ "^>"){match($0, />kraken:taxid\|([0-9]+)/,a); sub(/kraken:taxid\|/, "", a[0]); print a[0]} else {print $0}}' > temp.faa
+mv ../temp.faa viruses/kaiju_db_viruses.faa
 
-#cat library.fna | awk '{if ($0 ~ "^>"){match($0, />kraken:taxid\|([0-9]+)/,a); sub(/kraken:taxid\|/, "", a[0]); print a[0]} else {print $0}}' > temp.faa
-#mv ../temp.faa viruses/kaiju_db_viruses.faa
-
-# if Kaiju was installed locally
-#~/csf_mNGS/kaiju/bin/kaiju-makedb -t 4 --index-only -s viruses
+ if Kaiju was installed locally
+~/csf_mNGS/kaiju/bin/kaiju-makedb -t 4 --index-only -s viruses
 
